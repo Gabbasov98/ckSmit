58f6f0707af19156d320255442b67f7ceb2a4b4c
Marsel Gabbasov <marsel13_98@mail.ru>
2021-10-10 14:50:03 +0300

    services

---
+++ footer.php
@@ -0,0 +1,74 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="df">
+                    <div class="el el1">
+                        <a href="/" class="flogo">
+                            <img src="images/logo.png" alt="">
+                        </a>
+                    </div>
+                    <div class="el el2">
+                        <p>
+                            © 2003-2020 ООО "Центр комплектации<br>"Строительные материалы и технологии"
+                        </p>
+                    </div>
+
+                    <div class="el el3">
+                        <ul>
+                            <li><a href="/company/delivery/">ДОСТАВКА</a></li>
+                            <li><a href="/catalog/">КАТАЛОГ</a></li>
+                            <li><a href="/company/">КОМПАНИЯ</a></li>
+                            <li><a href="/contacts/">КОНТАКТЫ</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="el el4">
+                        <a href="https://www.instagram.com/ck_smit/" onclick="ym(62908210,'reachGoal','insta'); return true;" class="socin" target="_blank"></a>
+                        <a href="https://vk.com/ck_smit" onclick="ym(62908210,'reachGoal','vk'); return true;" class="socvk" target="_blank"></a>
+                        <a href="https://wa.me/79147060395" onclick="ym(62908210,'reachGoal','wa'); return true;" class="socwa" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="df df2">
+                    <div class="el">
+                        <p>
+                            Копирование материалов с сайта - запрещено
+                        </p>
+                    </div>
+                    <div class="el">
+                        <p>
+                            <a href="/privacy_policy/">Политика обработки персональной информации</a>
+                        </p>
+                    </div>
+                    <div class="el">
+                        <p>
+                            <a href="/info/">Ограничение ответственности</a>
+                        </p>
+                    </div>
+                    <div class="el el-research">
+                        <p>
+                            <a href="https://kefirok.ru/" target="_blank">Разработка сайта - Студия Кефирок 2020 год</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/nouislider.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.4.0/adapter.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.inputmask.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script src='js/jquery.mCustomScrollbar.concat.min.js'></script>
+<script src="js/swiper.js"></script>
+<script src="//code-ya.jivosite.com/widget/qAFIDiTjlI" async></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="js/datepicker-ru.js"></script>
+<script src="js/script.js"></script>
+<script src="js/index.js"></script>
---
+++ head.php
@@ -0,0 +1,19 @@
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,500,600,700&display=swap&subset=cyrillic" rel="stylesheet">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="css/nouislider.min.css">
+  <link rel="stylesheet" href="css/nice-select.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel='stylesheet' href='css/jquery.mCustomScrollbar.css' />
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/blitzer/jquery-ui.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style-new.css">    
+  <link rel="stylesheet" href="css/responsive.css">
+  
+  
